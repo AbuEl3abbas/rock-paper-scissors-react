90057473034f100e030c6bd9403fa70d7d97b683
AbuEl3abbas <marwan20000@live.com>
2022-05-20 02:07:55 +0300

    github url edited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
     return (
         <footer>
-            <a href="https://github.com/AbuEl3abbas/" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/AbuEl3abbas/rock-paper-scissors-react" target="_blank" rel="noopener noreferrer">
                 <h4>Made by AbuEl3abbas</h4>
             </a>
         </footer>
